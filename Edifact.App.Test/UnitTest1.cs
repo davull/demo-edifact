f20b417ea1f68ce6c5f531f566c46daf1d130a9e
David Ullrich <david.ullrich@ullrich-software.de>
2023-11-26 11:41:09 +0100

    Add sln

---
+++ Edifact.App.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Edifact.App.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
